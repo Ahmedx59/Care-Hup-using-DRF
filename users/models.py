4be32c93adf1b284d6af217e743f56508926dd2e
Ahmedx59 <Ahmedx59@gmail.com>
2024-12-13 16:24:06 +0200

    update models , add gitignore and add static and media in setting

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     username = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
@@ -12,14 +13,19 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
+    def __str__(self):
+        return self.username
 
 
-class Doctor(AbstractUser):
+
+class DoctorNurseProfile(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE , related_name='doctor_profile')
     price = models.IntegerField()
     experience_year = models.IntegerField()
     about = models.TextField(max_length=500)
     certificates = models.TextField(max_length=500)
 
 
-class Patient(AbstractUser):
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE , related_name='patin_profile')
     chronic_diseases = models.TextField()
